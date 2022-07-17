dd48c69b56b513c19fd9c896516654656e4bafa8
Aayush Anurag <aayushanurag18la@gmail.com>
2022-07-17 14:08:04 +0530

    completed modal

--- Modal/script.js
+++ Modal/script.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $(".clicks").on("click", function () {
     $("#box").addClass("show");
   });
+
+  $(".submission").on("click", function () {
+    $("#box").removeClass("show");
+  });
 });
